609cdcf3337d0db6cc53ea5567e44c53a05984b3
ramzeth <ramzeth25@gmail.com>
2026-04-13 10:47:21 +0300

    Solution_3

--- services/movie_session.py
+++ services/movie_session.py
@@ -11,8 +11,8 @@ def create_movie_session(
 ) -> Optional[MovieSession]:
     new_movie_session = MovieSession.objects.create(
         show_time=movie_show_time,
-        movie=movie_id,
-        cinema_hall=cinema_hall_id,
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id,
         )
     return new_movie_session
 
@@ -38,7 +38,7 @@ def update_movie_session(
         movie_session.movie_id=movie_id
         movie_session.save()
     if cinema_hall_id:
-        movie_session.cinema_hall=cinema_hall_id
+        movie_session.cinema_hall_id=cinema_hall_id
         movie_session.save()
     return movie_session
 
