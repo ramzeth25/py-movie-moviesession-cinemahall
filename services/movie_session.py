76ec71b49fd5b989cbafeabec7b106bb06607b60
ramzeth <ramzeth25@gmail.com>
2026-04-13 10:37:03 +0300

    Solution_2

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,17 +1,18 @@
-import datetime
+from datetime import datetime
 from django.db.models import QuerySet
+from typing import Optional
 
 from db.models import MovieSession, Movie, CinemaHall
 
 def create_movie_session(
-        movie_show_time: datetime.datetime,
+        movie_show_time: datetime,
         movie_id: int,
         cinema_hall_id: int
-) -> MovieSession | None:
+) -> Optional[MovieSession]:
     new_movie_session = MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=movie_id,
-        movie=cinema_hall_id
+        movie=movie_id,
+        cinema_hall=cinema_hall_id,
         )
     return new_movie_session
 
@@ -26,16 +27,19 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(
         session_id: int,
-        show_time: str = None,
+        show_time: datetime = None,
         movie_id: int = None,
         cinema_hall_id: int = None) -> MovieSession:
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time:
         movie_session.show_time=show_time
+        movie_session.save()
     if movie_id:
         movie_session.movie_id=movie_id
+        movie_session.save()
     if cinema_hall_id:
         movie_session.cinema_hall=cinema_hall_id
+        movie_session.save()
     return movie_session
 
 def delete_movie_session_by_id(session_id: int) -> MovieSession:
